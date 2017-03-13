ecfa92700f77580bba3cb703e761da34193e0de4
Jamie Garner <jamiegarner123@hotmail.co.uk>
2017-03-13 01:14:19 +0000

    Add recipe pagination

--- src/components/recipe-list/index.js
+++ src/components/recipe-list/index.js
@@ -9,7 +9,9 @@ class RecipeList extends React.Component {
     super();
     this.state = {
       allRecipes: [],
-      displayedRecipes: [],
+      currentPageRecipes: [],
+      currentPage: 0,
+      recipesPerPage: 10,
       recipesLoaded: false
     };
   }
@@ -23,23 +25,31 @@ class RecipeList extends React.Component {
       } else if (response) {
         this.setState({
           allRecipes: response.body,
-          displayedRecipes: response.body,
+          currentPageRecipes: this.pageRecipes(response.body),
           recipesLoaded: true
         });
       }
     });
   }
 
+  pageRecipes(recipes) {
+    let currentPage = this.state.currentPage;
+    let recipesPerPage = this.state.recipesPerPage;
+
+    return recipes.slice(
+      currentPage * recipesPerPage,
+      currentPage * recipesPerPage + recipesPerPage
+    );
+  }
+
   filterRecipes(filterString) {
-    if (filterString) {
-      this.setState({
-        displayedRecipes: this.getFilteredRecipes(filterString)
-      });
-    } else {
-      this.setState({
-        displayedRecipes: this.state.allRecipes
-      });
-    }
+    const filteredRecipes = filterString ?
+      this.getFilteredRecipes(filterString) :
+      this.state.allRecipes;
+
+    this.setState({
+      currentPageRecipes: this.pageRecipes(filteredRecipes)
+    });
   }
 
   getFilteredRecipes(filterString) {
@@ -73,10 +83,10 @@ class RecipeList extends React.Component {
   }
 
   renderRecipes() {
-    if (this.state.displayedRecipes.length > 0) {
+    if (this.state.currentPageRecipes.length > 0) {
       return (
         <ul> {
-          this.state.displayedRecipes.map((recipe, key) => {
+          this.state.currentPageRecipes.map((recipe, key) => {
             return (
               <RecipeListItem recipe={recipe} key={key} />
             );
