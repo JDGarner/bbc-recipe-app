20e29b8700d9fc599389d9a4f62a1c2d4069c261
Jamie Garner <jamiegarner123@hotmail.co.uk>
2017-03-15 00:19:35 +0000

    Display ingredients and image on recipe page

--- src/components/config/index.js
+++ src/components/config/index.js
@@ -1,4 +1,5 @@
 const config = {
+  "cdnHost": "https://s3.eu-west-2.amazonaws.com/bbc-recipe-images/",
   "recipeApiHost": "https://bbc-recipe-api.herokuapp.com"
   // "recipeApiHost": "http://localhost:3001"
 };
--- src/components/views/recipe/index.js
+++ src/components/views/recipe/index.js
@@ -30,16 +30,32 @@ export default class Recipe extends React.Component {
 
   render() {
     if (this.state.recipeLoaded) {
+      let imgPath = config.cdnHost + this.state.recipe.imageurl;
+
       return (
         <div className="recipe">
           <h3>Name: {this.state.recipe.name}</h3>
           <h3>Cooking Time: {this.state.recipe.cookingtime} Minutes</h3>
-          <h3>Main Ingredients: {this.state.recipe.mainingredients}</h3>
+          <img src={imgPath}/>
+          <h3>Ingredients:</h3>
+          { this.renderIngredients(this.state.recipe.ingredients) }
           <Link to="/">Back to List</Link>
         </div>
       );
     } else {
       return null;
     }
+  }
+
+  renderIngredients(ingredients) {
+    return (
+      <ul className="ingredients-list"> {
+        ingredients.map((i, key) => {
+          return (
+            <li key={key} >{i.quantity} {i.quantityType} {i.name}</li>
+          );
+        })
+      } </ul>
+    );
   }
 }
